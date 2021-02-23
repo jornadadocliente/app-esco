5c0c33ca5713f4db334c358e4282579f901016a9
Tiago Fernandes Lima Neto <tiagof7@MacBook-Pro-de-Tiago.local>
2021-02-23 15:31:49 -0300

    Setting global style

---
+++ src/styles/global.js
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    scroll-behavior: smooth;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat';
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  .container {
+    width: 100%;
+    max-width: 1200px;
+    padding: 0 16px;
+    display: flex;
+    margin: 0 auto;
+    flex-direction: column;
+  }
+
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .only-desktop {
+    display: block;
+    @media screen and (max-width: 800px) {
+      display: none;
+    }
+  }
+
+  .only-mobile {
+    display: none;
+    @media screen and (max-width: 800px) {
+      display: block;
+    }
+  }
+
+`
+
+export default GlobalStyle;
