610e9b47d8fbbc8d50ecaea90a0980026a440ab5
Tiago Fernandes Lima Neto <lobotiago>
2021-03-04 10:08:43 -0300

    Dashboard screen complete without API

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,6 +10,11 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import SearchIcon from '@material-ui/icons/Search';
 import TuneIcon from '@material-ui/icons/Tune';
 
+import Bascula from '../images/bascula.png'
+import ChapaDeDesgaste from '../images/chapa-de-desgaste.png'
+import Gets from '../images/gets.png'
+import KwikLok from '../images/kwik-lok.png'
+
 const CssTextField = withStyles({
   root: {
     '& .MuiInputBase-root': {
@@ -66,6 +71,45 @@ function Dashboard() {
             <TuneIcon /> Filtro Avançado
           </Link>
         </Filters>
+
+        <div className="row">
+          <Categories>
+            <Link to="/">
+              <img src={ Bascula } alt="Báscula" />
+              <h3>Báscula</h3>
+              <button>
+                Ver Mais
+              </button>
+            </Link>
+          </Categories>
+          <Categories>
+            <Link to="/">
+              <img src={ ChapaDeDesgaste } alt="Báscula" />
+              <h3>Chapa de Desgaste</h3>
+              <button>
+                Ver Mais
+              </button>
+            </Link>
+          </Categories>
+          <Categories>
+            <Link to="/">
+              <img src={ Gets } alt="Báscula" />
+              <h3>Gets</h3>
+              <button>
+                Ver Mais
+              </button>
+            </Link>
+          </Categories>
+          <Categories>
+            <Link to="/">
+              <img src={ KwikLok } alt="Báscula" />
+              <h3>Kwik Lok</h3>
+              <button>
+                Ver Mais
+              </button>
+            </Link>
+          </Categories>
+        </div>
       </Container>
 
     </div>
@@ -129,6 +173,110 @@ const Filters = styled.div`
 
     &:hover {
       background-color: #04345526;
+    }
+  }
+
+  @media screen and (max-width: 766px) {
+    flex-wrap: wrap;
+    .MuiFormControl-root {
+      width: 100%;
+    }
+
+    a {
+      width: 100%;
+      justify-content: center;
+    }
+  }
+`
+
+const Categories = styled.div`
+  width: calc(50% - 32px);
+  height: 240px;
+  margin: 8px 16px;
+  border-radius: 10px;
+  background-color: #C4C4C4;
+  overflow: hidden;
+  position: relative;
+
+  a {
+    width: 100%;
+    height: 100%;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    transition: all 0.4s;
+  }
+
+  h3 {
+    color: #FFF;
+    font-size: 40px;
+    font-weight: 600;
+    position: absolute;
+    bottom: 30px;
+    left: 30px;
+    max-width: 60%;
+    line-height: 40px;
+  }
+
+  button {
+    position: absolute;
+    bottom: 30px;
+    right: 30px;
+    background: #BC080D;
+    color: #FFF;
+    border: none;
+    padding: 16px 24px;
+    border-radius: 4px;
+    font-size: 14px;
+    transition: all 0.3s;
+
+    &:hover {
+      background: #8c0509;
+    }
+  }
+
+  &:hover {
+    img {
+      transform: scale(1.1);
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    h3 {
+      font-size: 32px;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    width: calc(100% - 32px);
+  }
+
+  @media screen and (max-width: 414px) {
+    h3 {
+      font-size: 28px;
+      left: 8px;
+      line-height: 1;
+    }
+
+    button {
+      right: 8px;
+    }
+  }
+
+  @media screen and (max-width: 375px) {
+    margin: 8px 0;
+    width: 100%;
+    height: 160px;
+
+    h3 {
+      font-size: 22px;
+    }
+
+    button {
+      padding: 8px;
     }
   }
 `
